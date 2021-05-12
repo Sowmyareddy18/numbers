108a7d27a79ded16a174d8c492d5314c3c8b8256
Sowmyareddy18 <82565407+Sowmyareddy18@users.noreply.github.com>
2021-05-12 09:41:03 +0530

    Add files via upload

---
+++ prime.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int n,count;
+	cin>>n;
+	for(int i=1;i<=n;i++)
+	{
+		if(n%i==0)
+		{
+			count++;
+		}
+	}
+	if(count==2)
+	cout<<"prime";
+	else
+	cout<<"not prime";
+	return 0;
+}
+
