7acba6df51b5b54ae3a8d6e864dfa547adfd05ef
Sowmyareddy18 <82565407+Sowmyareddy18@users.noreply.github.com>
2021-05-19 09:36:27 +0530

    Add files via upload

---
+++ Gcd_Euclids.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int a,b;
+	cin>>a>>b;
+	while(1)
+	{
+		if(a<b)
+		   swap(a,b);
+		a=a%b;
+		if(a==0)
+		{
+			cout<<b;
+			break;
+		}
+	}
+	return 0;
+}
